310c8c7e00c74efa55381aecc0b4653f1166f697
jakepreciado <jacobpreciado@gmail.com>
2026-02-04 18:48:59 -0700

    Simple styling update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
 import { useRouter } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import { Button, Image, Text, View } from 'react-native';
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-      <Text style={{ fontSize: 50, textAlign: "center" }}>Welcome to DishUp!</Text>
-      <Text style={{ fontSize: 20, marginBottom: 75, marginTop: 10 }}> V1.0.0</Text>
-      <Button title="Go to Pantry" onPress={() => router.push('/pantry')} />
-      <Button title="Go to Recipes" onPress={() => router.push('/recipes')} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Image
+        source={require('../../assets/images/dishup-logo.png')}
+        style={{ width: 450, height: 450 }} />
+      
+      <Text style={{ fontSize: 36, marginBottom: 25 }}>Welcome!</Text>
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 20 }}>
+        <Button title="Go to Pantry" onPress={() => router.push('/pantry')} />
+        <Button title="Go to Recipes" onPress={() => router.push('/recipes')} />
+      </View>
+      <Text style={{ fontSize: 16, marginTop: 20 }}> V1.0.0</Text>
     </View>
   );
 }
